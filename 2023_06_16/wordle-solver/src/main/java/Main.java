3d55395bcede1fac8bcfc8aa778899f33bf35601
lgf10 <lukas.goenner@faktorzehn.de>
2023-06-16 10:02:07 +0200

    Dojo 2023-06 Update

---
+++ 2023_06_16/wordle-solver/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Wordle wordle = new Wordle();
+		
+		System.out.println(wordle.guess("crane"));		
+	}
+
+}
--- 2023_06_16/wordle-solver/src/main/java/Wordle.java
+++ 2023_06_16/wordle-solver/src/main/java/Wordle.java
@@ -1,22 +1,50 @@
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
 
 public class Wordle {
 
-	public static void main(String[] args) {
-		System.out.println(validate("cards"));
-	}	
+	public static final List<String> VALID_GUESSES = readLines("valid-guesses.txt");
+	public static final List<String> POSSIBLE_ANSWERS = readLines("possible-answers.txt");
 	
-	private static Result validate(String word) {
-		String expected = "crane";
+	private final String answer;
+	private int guesses = 0;
+	
+	public Wordle() {
+		answer = POSSIBLE_ANSWERS.get(new Random().nextInt(POSSIBLE_ANSWERS.size()));
+	}
+	
+	private static List<String> readLines(String file) {
+		try {
+			URL resource = Wordle.class.getResource(file);
+			return Files.readAllLines(Paths.get(resource.toURI()));
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to load " + file, e);
+		}
+	}
+	
+	public Result guess(String word) {
+		guesses++;
+		if(!POSSIBLE_ANSWERS.contains(word)) {
+			throw new IllegalArgumentException("Invalid word: " + word);
+		}
 		
 		Result result = new Result(word);
 		for(int i = 0; i < 5; i++) {
-			if(expected.contains(String.valueOf(word.charAt(i)))) {
+			if(answer.contains(String.valueOf(word.charAt(i)))) {
 				result.setLetterContained(i, true);
 			}
-			if(expected.charAt(i) == word.charAt(i)) {
+			if(answer.charAt(i) == word.charAt(i)) {
 				result.setLetterCorrect(i, true);
 			}
 		}
 		return result;
 	}
+	
+	public int getGuesses() {
+		return guesses;
+	}
+	
 }
